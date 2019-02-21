5648608fa7e2f8916b8da7185cd4c680e1ee56ca
Joseph Oxborrow <j@climatejourney.com>
2019-02-20 18:53:10 -0600

    initial commit

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="master-wrap"> <!--master-wrap -->
+
+<div id="page" class="site front">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+
+	<header id="masthead" class="site-header">
+
+      <div id="menu-toggle" class="toggleBox">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+	  </div>
+	  
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
